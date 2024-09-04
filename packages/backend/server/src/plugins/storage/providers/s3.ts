ce039f3eb122527bd04840a1598c6b6729321dee
timonen87 <timonen.as@gmail.com>
2024-09-04 17:52:09 +0300

    add fix 18

--- packages/backend/server/src/plugins/storage/providers/s3.ts
+++ packages/backend/server/src/plugins/storage/providers/s3.ts
@@ -44,11 +44,12 @@ export class S3StorageProvider implements StorageProvider {
 
     this.client = new S3Client({
       ...config,
-      endpoint: 'https://fine-app.hb.ru-msk.vkcloud-storage.ru',
+      endpoint: `https://${bucket}.hb.ru-msk.vkcloud-storage.ru`,
       region: 'ru-msk',
       apiVersion: 'latest',
     });
     this.logger = new Logger(`${S3StorageProvider.name}:${bucket}`);
+    console.log(this.client);
   }
 
   async put(
