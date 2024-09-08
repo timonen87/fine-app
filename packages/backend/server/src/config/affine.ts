0765a2dab08500c5cf6cbe78f819b8273554d76f
timonen87 <timonen.as@gmail.com>
2024-09-08 22:36:53 +0300

    add fix image

--- packages/backend/server/src/config/affine.ts
+++ packages/backend/server/src/config/affine.ts
@@ -145,40 +145,40 @@ AFFiNE.server.port = 3010;
 // AFFiNE.storages.avatar.provider = 'cloudflare-r2';
 //
 // /* OAuth Plugin */
-// AFFiNE.use('oauth', {
-//   providers: {
-//     github: {
-//       clientId: '',
-//       clientSecret: '',
-//       // See https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
-//       args: {
-//         scope: 'user',
-//       },
-//     },
-//     google: {
-//       clientId: '',
-//       clientSecret: '',
-//       args: {
-//         // See https://developers.google.com/identity/protocols/oauth2
-//         scope: 'openid email profile',
-//         promot: 'select_account',
-//         access_type: 'offline',
-//       },
-//     },
-//     oidc: {
-//       // OpenID Connect
-//       issuer: '',
-//       clientId: '',
-//       clientSecret: '',
-//       args: {
-//         scope: 'openid email profile',
-//         claim_id: 'preferred_username',
-//         claim_email: 'email',
-//         claim_name: 'name',
-//       },
-//     },
-//   },
-// });
+AFFiNE.use('oauth', {
+  providers: {
+    github: {
+      clientId: '',
+      clientSecret: '',
+      // See https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+      args: {
+        scope: 'user',
+      },
+    },
+    google: {
+      clientId: '',
+      clientSecret: '',
+      args: {
+        // See https://developers.google.com/identity/protocols/oauth2
+        scope: 'openid email profile',
+        promot: 'select_account',
+        access_type: 'offline',
+      },
+    },
+    oidc: {
+      // OpenID Connect
+      issuer: '',
+      clientId: '',
+      clientSecret: '',
+      args: {
+        scope: 'openid email profile',
+        claim_id: 'preferred_username',
+        claim_email: 'email',
+        claim_name: 'name',
+      },
+    },
+  },
+});
 //
 // /* Copilot Plugin */
 // AFFiNE.use('copilot', {
