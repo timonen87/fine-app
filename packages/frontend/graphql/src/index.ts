3924ad62a9efde72adac737106b76f1f6b3ace9a
timonen87 <timonen.as@gmail.com>
2024-09-10 13:47:40 +0300

    add dev 2323ded9fe

--- packages/frontend/graphql/src/index.ts
+++ packages/frontend/graphql/src/index.ts
@@ -17,8 +17,8 @@ export function getBaseUrl(): string {
     // is nodejs
     return '';
   }
-  const { protocol, hostname, port } = window.location;
-  return `${protocol}//${hostname}${port ? `:${port}` : ''}`;
+  const { protocol } = window.location;
+  return `${protocol}//${process.env.AFFINE_SERVER_HOST}${process.env.AFFINE_SERVER_PORT ? `:${process.env.AFFINE_SERVER_PORT}` : ''}`;
 }
 
 export const fetcher = gqlFetcherFactory(getBaseUrl() + '/graphql');
